ca193ffe8ce0edc95c587002b0ecfdfd7c6deef6
Josty <josty.qweenq@gmail.com>
2021-12-01 20:36:17 +0700

    Added decoding data

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
 const net = require('net') // docs: https://nodejs.org/dist/latest-v14.x/docs/api/net.html
-const dns = require('dns') // docs: 
-const os = require('os') // docs: 
 const config = require('./config.json') // импортирум конфиг
 
 class Server extends net.Server{
@@ -8,6 +6,7 @@ class Server extends net.Server{
 		super(props)
 
 		this.on('connection', (socket) => {
+
 			this.socketEvents(socket)
 		})
 		this.on('listening', () => {
@@ -19,14 +18,14 @@ class Server extends net.Server{
 	socketEvents(socket){
 		socket
 			.on('error', (err) => console.error(err)) // при ошибке сокета выводим в консоль
-			.on('connect', () => { // при новом подключении
-				console.log(`Новое подключение: ${socket.address}`) //показываем ip
-			})
+
 			.on('data', (data) => {
-				console.log(data)
+				console.log(`Сообщение от:`, socket.address())
+				console.log(data.toString('utf-8'))
 			})
-			.on('disconnect', () => {
-				console.log('disconnect')
+
+			.on('end', () => {
+				console.log(socket.address(), 'Соединение прервано')
 			})
 	}
 	
@@ -34,7 +33,7 @@ class Server extends net.Server{
 
 new Server()
 	.listen({ // биндим сервер на host и port
-		address: 'localhost',
+		address: config.address || 'localhost',
 		port: config.port || 2550,
 		exclusive: true
 	})
